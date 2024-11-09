0109fd3b5644f4f2f95c161fed71fa152d7ea2f2
Maksim <maxim05gromov@yandex.ru>
2024-11-09 15:44:17 +0300

    Initial Commit

---
+++ HW41/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW41
+//
+//  Created by Максим Громов on 09.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
